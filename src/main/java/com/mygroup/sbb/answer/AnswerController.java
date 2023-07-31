472da81ae0173892c2409eadb050dccf6a68b85a
ji-min-choi <4metree@naver.com>
2023-08-01 00:43:58 +0900

    add answer anchor

--- src/main/java/com/mygroup/sbb/answer/AnswerController.java
+++ src/main/java/com/mygroup/sbb/answer/AnswerController.java
@@ -33,16 +33,17 @@ public class AnswerController {
 
     @PreAuthorize("isAuthenticated()")
     @PostMapping("/create/{id}")
-    public String createAnswer(Model model, @PathVariable("id") Integer id,  @Valid AnswerForm answerForm, BindingResult bindingResult, Principal principal) {
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @Valid AnswerForm answerForm, BindingResult bindingResult, Principal principal) {
         Question question = this.questionService.getQuestion(id);
         SiteUser siteUser = this.userService.getUser(principal.getName());
-
         if (bindingResult.hasErrors()) {
             model.addAttribute("question", question);
             return "question_detail";
         }
-        this.answerService.create(question, answerForm.getContent(), siteUser);
-        return String.format("redirect:/question/detail/%s", id);
+        Answer answer = this.answerService.create(question, 
+                answerForm.getContent(), siteUser);
+        return String.format("redirect:/question/detail/%s#answer_%s", 
+                answer.getQuestion().getId(), answer.getId());
     }
 
     @PreAuthorize("isAuthenticated()")
