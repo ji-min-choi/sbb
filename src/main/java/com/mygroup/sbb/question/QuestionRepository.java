9eec02397e3496374b3200407e65a1701768db5b
ji-min-choi <4metree@naver.com>
2023-08-01 01:26:40 +0900

    add question search function

--- src/main/java/com/mygroup/sbb/question/QuestionRepository.java
+++ src/main/java/com/mygroup/sbb/question/QuestionRepository.java
@@ -5,10 +5,12 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {  // 앞이 엔티티 뒤가 데이터 타입
     Question findBySubject(String subject);
     Question findBySubjectAndContent(String subject, String content);
     List<Question> findBySubjectLike(String subject);
     Page<Question> findAll(Pageable pageable);
+    Page<Question> findAll(Specification<Question> spec, Pageable pageable);
 }
