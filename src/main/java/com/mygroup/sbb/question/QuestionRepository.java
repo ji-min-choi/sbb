523d9c11997d8e8a9a94464b40b33c72884cc406
ji-min-choi <4metree@naver.com>
2023-07-31 11:56:15 +0900

    apply pagination

--- src/main/java/com/mygroup/sbb/question/QuestionRepository.java
+++ src/main/java/com/mygroup/sbb/question/QuestionRepository.java
@@ -3,8 +3,12 @@ package com.mygroup.sbb.question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface QuestionRepository extends JpaRepository<Question, Integer> {  // 앞이 엔티티 뒤가 데이터 타입
     Question findBySubject(String subject);
     Question findBySubjectAndContent(String subject, String content);
     List<Question> findBySubjectLike(String subject);
+    Page<Question> findAll(Pageable pageable);
 }
